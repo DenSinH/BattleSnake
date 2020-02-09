286095b1f07356090f1ab6e2f3771020cd32679b
Dennis <randomspam1906@gmail.com>
2020-02-08 16:53:20 -0800

    changed index

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from api import ping_response, start_response, move_response, end_response
 @bottle.route('/')
 def index():
     with open(os.path.join(os.path.dirname(__file__), "snake.py"), "r") as f:
-        return f.read()
+        return f
 
 
 @bottle.route('/static/<path:path>')
