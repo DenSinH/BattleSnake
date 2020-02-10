0459680fddfa03b2cb4d72002217e62be87beba6
Dennis <randomspam1906@gmail.com>
2020-02-10 10:51:33 -0800

    added connected component detection

--- app/snake.py
+++ app/snake.py
@@ -1,9 +1,13 @@
+from pprint import pprint
+
+
 dirs = {
     (1, 0): "right",
     (-1, 0): "left",
     (0, 1): "down",
     (0, -1): "up"
 }
+
 
 def manhattan(point1, point2):
     return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
@@ -34,6 +38,11 @@ class Path(object):
     def get(self):
         return dirs.get(self.firstdir)
 
+    def first_head(self):
+        if len(self.path) < 2:
+            return None
+        return self.path[1]
+
     def dist(self, point):
         return len(self) + manhattan(self.end, point)
 
@@ -44,6 +53,9 @@ class Snake(object):
         self.health = health
         self.body = [(pos["x"], pos["y"]) for pos in body]
         self.head = self.body[0]
+
+    def __len__(self):
+        return len(self.body)
 
     def strength(self):
         return len(self.body)
@@ -84,23 +96,49 @@ class Game(object):
     def score(self, path):
         s = 0
         for spot in path:
-            # we dont want to move next to a snakes head
+            # we dont want to move next to another snakes head
             if any(manhattan(path.end, snake.head) == 1 for snake in self.snakes):
                 return -1
 
+            # snake likes to be next to game border
             if spot[0] in [0, self.width - 1]:
                 s += 1
 
             if spot[1] in [0, self.height - 1]:
                 s += 1
 
+            # snake likes to be next to own body even more
             if spot in self.you.body:
                 s += 5
 
+            # snake likes to be next to other snakes too sort of
             elif any(spot in snake.body for snake in self.snakes):
                 s += 3
 
         return s
+
+    def components(self, *extra):
+        walls = {part for snake in self.snakes for part in snake.body} | set(self.you.body) | set(extra)
+        components = []
+
+        for x in range(self.width):
+            for y in range(self.height):
+                point = (x, y)
+                if point in walls:
+                    continue
+
+                new_components = []
+                current = {(x, y)}
+
+                for component in components:
+                    if any(manhattan(point, p) == 1 for p in component):
+                        current |= component
+                    else:
+                        new_components.append(component)
+
+                components = new_components + [current]
+
+        return components
 
     def move(self):
         found = set(self.you.head)
@@ -118,7 +156,14 @@ class Game(object):
                     return next_path.get()
 
                 if next_path.end in self.food:
-                    shortest_paths.append(next_path)
+                    components = self.components(next_path.first_head())
+                    pprint(components)
+                    for component in components:
+                        if next_path.end in component and len(component) < len(self.you):
+                            print(f"Did not allow path to {next_path.end} because component too small")
+                            break
+                    else:
+                        shortest_paths.append(next_path)
 
                 if next_path.end not in found:
                     found.add(next_path.end)
